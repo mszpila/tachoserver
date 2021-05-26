dae9b137bf946f145aba8a869c2c126b2e2c999c
mszpila <szpila.mikolaj@gmail.com>
2021-05-26 18:37:00 +0200

    Add CORS whitelist

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 import * as cookieParser from 'cookie-parser';
 import * as helmet from 'helmet';
-import * as csurf from 'csurf';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  const configService = app.get(ConfigService);
+  app.enableCors({
+    origin: configService.get<string>('CORS_WHITELIST').split(','),
+  });
   app.use(cookieParser());
   app.use(helmet());
-  app.use(csurf());
-  await app.listen(3000);
+  await app.listen(configService.get<number>('PORT'));
 }
 bootstrap();
